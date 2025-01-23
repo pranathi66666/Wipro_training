d0f66873b68f3f3992fe93cd80b4be0e4c19bf82
pranathi66666 <110125089+pranathi66666@users.noreply.github.com>
2025-01-23 15:09:43 +0530

    Add files via upload

---
+++ Withoutreturn.java
@@ -0,0 +1,20 @@
+public class Withoutreturn {
+
+    public static void main(String[] args) {
+		        int a = 10;
+		        int b = 2;
+
+		        int sum = a + b;
+		        System.out.println("Addition: " + sum);
+
+		        int difference = a - b;
+		        System.out.println("Subtraction: " + difference);
+
+		        int product = a * b;
+		        System.out.println("Multiplication: " + product);
+
+		        int division = a / b;
+		        System.out.println("Division: " + division);
+	}
+    
+}
