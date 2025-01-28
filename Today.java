2ac4d53d5edb0d0caecf5f544ad6176d17faf05b
pranathi66666 <110125089+pranathi66666@users.noreply.github.com>
2025-01-28 11:16:02 +0530

    Today.java
    Printing Current Date

---
+++ Today.java
@@ -0,0 +1,12 @@
+package com.wipro;
+import java.time.LocalDate;
+public class Today {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LocalDate today=LocalDate.now();
+		System.out.println(today);
+
+	}
+
+}
