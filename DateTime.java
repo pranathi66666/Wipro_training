e52e7be10d26166bbf81682876dcc1bccdff3754
pranathi66666 <110125089+pranathi66666@users.noreply.github.com>
2025-01-28 11:17:08 +0530

    DateTime.java
    Print Curent Date and Time

---
+++ DateTime.java
@@ -0,0 +1,11 @@
+package com.wipro;
+import java.time.LocalDateTime;
+public class DateTime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        LocalDateTime t=LocalDateTime.now();
+        System.out.println(t);
+	}
+
+}
